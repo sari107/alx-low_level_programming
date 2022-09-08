ef0be14762997fd477de925220696d398812780b
Saron <saronkedir@gmail.com>
2022-09-08 04:50:45 -0700

    fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return:- 0
+ */
 int main(void){
-	puts("\"Programming is like building a multilingual Puzzle");
+	puts("\ "Programming is like building a multilingual Puzzle");
 	return (0);
 }
