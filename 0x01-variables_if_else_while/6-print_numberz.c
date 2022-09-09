13c655d3cb893cfcaca1c6c84791445579940512
Saron <saronkedir@gmail.com>
2022-09-09 00:26:48 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the main block
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		purchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (o);
+}
