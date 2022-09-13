48b94658bd30b308a083321880ce63e8fb22d016
Saron <saronkedir@gmail.com>
2022-09-13 05:44:00 -0700

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	n = n >= 0 ? n : n * -1;
-	int last = n % 10;
-	_putchar('0' + last);
-	return (last);
+	n = n % 10;
+
+	if (n < 0)
+		n = -n;
+	_putchar(n + '0');
+	return (n);
 }
