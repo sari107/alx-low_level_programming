48f081ee3ade23ad813b7e6a92ffc3d4259cd920
Saron <saronkedir@gmail.com>
2022-09-13 03:25:29 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.h"
+/**
+ * main - prints _putchar
+ * Return - alway 0 (success)
+ */
+int main(void)
+{
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+	_putchar(c[i]);
+	i++;
+}
+_putchar('\n');
+return (0);
+}
