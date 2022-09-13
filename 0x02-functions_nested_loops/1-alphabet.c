b1c3737f51a0c65b163dfdf9cebca34d0086d251
Saron <saronkedir@gmail.com>
2022-09-13 04:00:32 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - the function that hold the code
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+	while (alph <= 'z')
+	{
+		_putchar (alph);
+		alph++;
+	}
+	_putchar('\n');
+}
