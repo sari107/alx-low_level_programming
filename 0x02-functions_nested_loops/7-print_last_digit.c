795fee2f5a938939fd7a45656a27094ebc9ccc42
Saron <saronkedir@gmail.com>
2022-09-13 05:40:49 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - return last digit
+ * @n : number to be checked
+ * Return:0 or 1
+ */
+int print_last_digit(int n)
+{
+	n = n >= 0 ? n : n * -1;
+	int last = n % 10;
+	_putchar('0' + last);
+	return (last);
+}
