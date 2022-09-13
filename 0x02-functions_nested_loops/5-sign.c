9504266b7bc5fedfa42c92364c0dfd7691e418d2
Saron <saronkedir@gmail.com>
2022-09-13 05:06:27 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		test = 1;
-
 		_putchar('+');
 	}
-	else if ( n == 0)
+	else if (n == 0)
 	{
 		test = 0;
-
 		_putchar('0');
 	}
 	else
 	{
 		test = -1;
-
 		_putchar('-');
 	}
 	
