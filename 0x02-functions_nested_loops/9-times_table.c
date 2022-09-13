2178d1ff38668295525ef8f7b8f89e1c400705c7
Saron <saronkedir@gmail.com>
2022-09-13 06:14:10 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,8 +12,26 @@ void times_table(void)
 		for (x = 0; x < 10; x++)
 		{
 			m_value = x * y;
-			_putchar(m_value);
-			_putchar(',');
+			if (x == 0)
+			{
+				_putchar(m_value + '0');
+			}
+			else if (m_value >= 10)
+			{
+				_putchar(' ');
+				_putchar(m_value / 10 + '0');
+				_putchar(m_value % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(m_value + '0');
+			}
+			if (x != 9)
+			{
+				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}
