1977d16d3358ce7834458221aeef243a506a6b2f
Saron <saronkedir@gmail.com>
2022-09-13 06:41:58 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i++){
+		for (i = n; i >= 98; i--){
 			if (i != n){
 				printf(", ");
 			}
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i <= 98; i--)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != n)
 			{
