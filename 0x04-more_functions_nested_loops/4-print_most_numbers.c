d643edbdef8893066e03ce835956a9f241f133fb
Saron <saronkedir@gmail.com>
2022-09-15 23:37:09 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	for (int i = 0; i < 10 ; i++)
+	int i;
+
+	for (i = 0; i < 10 ; i++)
 	{
 		if (i != 2 || i != 4)
 		{
 			_putchar('0' + i);
 		}
-	
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
