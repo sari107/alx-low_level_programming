ce083c7ddc04fb0cee8d738d9c999e6ccb976c8e
Saron <saronkedir@gmail.com>
2022-09-15 23:42:02 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
  * print_most_number - func
  * Descriptpion: Print number with _putchar
  * Return: void
  */
+
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10 ; i++)
+	for (i = '0'; i < '10' ; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i != '2' || i != '4')
 		{
 			_putchar(i);
 		}
