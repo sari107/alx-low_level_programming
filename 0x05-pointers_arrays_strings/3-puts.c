5c080502df2b61264ad862b5349965daadb05b73
Saron <saronkedir@gmail.com>
2022-09-19 01:28:28 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * -puts - func
+ *  @str - pointer
+ *  Return: void
+ */
+
+void _puts(char *str)
+{
+int j = 0;
+while (str[j])
+{
+	_putchar(str[j]);
+	j++;
+}
+_putchar('\n');
+}
