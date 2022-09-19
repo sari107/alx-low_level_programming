71d4a1ba98fc43ebdf7c9ac87f5c47896103ef63
Saron <saronkedir@gmail.com>
2022-09-19 01:38:56 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - func
+ * @s: pointer
+ * Return:void
+ */
+
+void print_rev(char *s)
+{
+int j = 0;
+
+while (s[j])
+	j++;
+
+while (i--)
+{
+	_putchar(s[i]);
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,3 +8,5 @@ int _strlen(char *s);
 
 void _puts(char *str);
 
+void print_rev(char *s);
+
