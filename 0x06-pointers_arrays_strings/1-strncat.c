e89262f227bf6cb1ce51d142c230a48a578f4e09
Saron <saronkedir@gmail.com>
2022-09-22 05:42:50 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,18 @@
 char *_strncat(char *dest, char *src, int n)
 
 {
-	int i = -1, j;
 
-	for(j=0; dest[j] <= n;j++)
+	int i =-1, j;
+
+	for (j=0; dest[j] <= n; j++)
 
 		do{
 			i++;
-			dest[j] = src [i];
+			dest[j] = src[i];
 			j++;
-		}while(src[i] !='\0');
-return (dest);
+		}while(src[i] != '\0');
+
+	  return (dest);
+
 }
 
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy -func
+ * @dest : pointer
+ * @src : pointer
+ * Return Void
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (src[n] !='\0')
+	{
+		dist[n] = src[n];
+		n++;
+	}
+	dist[n] ='\0';
+
+	return (dist);
+}
