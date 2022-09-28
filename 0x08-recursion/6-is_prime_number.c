5791353a02d5ab0850dbe2fd4f563e01356599d9
Saron <saronkedir@gmail.com>
2022-09-28 06:20:46 -0700

    TASK 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,17 +6,28 @@
 
 int is_prime_number(int n)
 {
-	int m = n / 2;
-	if(n <= 1)
+	int m;
+	m = 2;
+	if(n < 2)
+	{
 		return (0);
+	}
+	if(n == 2)
+	{
+		return (1);
+	}
 	return (is_prime(n,m));
 }
 
 int is_prime(int n, int m)
 {
-	if(m <= 1)
+	if(m == n -1)
 		retutn (1);
 	else if(n % m == 0)
 		return (0);
-	return (is_prime(n, m-1));
+	if(n % m != 0)
+	{
+	return (is_prime(n, m + 1));
+	}
+	return (0);
 }
