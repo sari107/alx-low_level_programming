a959ecfd1ce3a8f166021487f1836bbe8b647743
Saron <saronkedir@gmail.com>
2022-09-28 06:38:38 -0700

    TASK 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,8 +7,8 @@
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(0, n));
+		return(n);
+	return(_sqrt(0, n));
 }
 
 /**
@@ -18,9 +18,9 @@ int _sqrt_recursion(int n)
 
 int _sqrt(int n, int x)
 {
-	if (n > x/2)
-		return (-1);
+	if (n > x / 2)
+		return(-1);
 	else if (n * n == x)
-		return (n);	
-	return (_sqrt(n + 1, x));
+		return(n);	
+	return(_sqrt(n + 1, x));
 }	
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,28 +6,17 @@
 
 int is_prime_number(int n)
 {
-	int m;
-	m = 2;
-	if(n < 2)
-	{
+	int m = n / 2;
+	if(n <= 1)
 		return (0);
-	}
-	if(n == 2)
-	{
-		return (1);
-	}
 	return (is_prime(n,m));
 }
 
 int is_prime(int n, int m)
 {
-	if(m == n -1)
+	if(m <= 1)
 		retutn (1);
 	else if(n % m == 0)
 		return (0);
-	if(n % m != 0)
-	{
-	return (is_prime(n, m + 1));
-	}
-	return (0);
+	return (is_prime(n, m-1));
 }
