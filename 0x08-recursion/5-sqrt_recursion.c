fba16c53f5bea5e5803118c4066fbc1a2a3cdb46
Saron <saronkedir@gmail.com>
2022-09-28 06:40:51 -0700

    TASK 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,8 +7,8 @@
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
-		return(n);
-	return(_sqrt(0, n));
+		return (n);
+	return (_sqrt(0, n));
 }
 
 /**
@@ -19,8 +19,8 @@ int _sqrt_recursion(int n)
 int _sqrt(int n, int x)
 {
 	if (n > x / 2)
-		return(-1);
+		return (-1);
 	else if (n * n == x)
-		return(n);	
-	return(_sqrt(n + 1, x));
+		return (n);	
+	return (_sqrt(n + 1, x));
 }	
