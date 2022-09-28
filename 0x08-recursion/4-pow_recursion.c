4704dcb4987765a272ef310b1fa9ec8ffcc63fe3
Saron <saronkedir@gmail.com>
2022-09-28 04:10:48 -0700

    TASK 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,12 +4,12 @@
  * Return
  */
 
-int _pow_recursion (int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	else if (y != 0)
-		return (x * _pow_recursion(x , y-1));
+		return (x * _pow_recursion(x, y - 1));
 	else
 		return (1);
 }
