aa2563fe7a6f969addc82fa435890ac58c9e4fa8
sari107 <sari107@github.com>
2022-09-29 05:47:40 -0700

    fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,11 +2,11 @@
 /**
  * _print_rev_recursion - func
  * @s : string
- * Return : On success 
+ * Return : On success
  */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		 _print_rev_recursion(s + 1);
 		 _putchar(*s);
