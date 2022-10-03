e6bde927359c13a28e31cfa201c7bef8ef8c34ce
sari107 <sari107@github.com>
2022-10-03 00:42:18 -0700

    hot fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-#include <math.h>
-#include "main.h"
 
 /**
  * main - func
- * Return: 0
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	long int number;
+	long i, j;
 
-	number = 612852475143;
+	i = 612852475143;
 
-	if (isPrime(number) == 1)
+	for (j = 2; i > j; j++)
 	{
-		printf("%ld\n", number);
+		while (i % j == 0)
+		{
+			i = i / j;
+		}
 	}
-	else
-	{
-		printf("%ld\n", biggestFactor(number));
-	}
+	printf("%lu", y);
+	putchar('\n');
 	return (0);
 }
