02d23a38cdeb75f5ccaee646033ce1ae59069ec9
sari107 <sari107@github.com>
2022-10-04 06:04:55 -0700

    print the name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 /**
  * main functin
  * @ argc, argv
@@ -7,11 +8,9 @@
 
 int main(int argc, char* argv[])
 {
-	for(int i = 0; i < argc; i++)
-	{
-		printf("%s", argv[i]);
-	}
-	printf("\n");
+ 	(void) argc;
 
-return 0;
+	printf("%s\n", argv[0]);
+
+return (0);
 }
