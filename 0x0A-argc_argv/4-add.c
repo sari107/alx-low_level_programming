52935f5472127b15e01f1e2a754353b78bca887d
sari107 <sari107@github.com>
2022-10-05 04:19:24 -0700

    Hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	int sum = 0;
 	
-	if(argc <= 1)
+	if (argc <= 1)
 	{
 		printf("0\n");
 	}
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	else
 	{
 
-	for(int i=1;i < argc;i++)
+	for (int i=1; i < argc; i++)
 	{
 		if (atoi(argv[i]) != 0)
 		{
