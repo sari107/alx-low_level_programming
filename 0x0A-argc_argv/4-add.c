481c29b085887a7b2291d01feae9016bf01e0a86
sari107 <sari107@github.com>
2022-10-05 01:21:06 -0700

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,13 @@
-#include "main.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "ctype.h"
+#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 /**
- *function to add
- *@argc
- *@argv
- *return 1 if error
+ * main - function to add
+ * Return: 0
  */
+
 
 int main(int argc, char *argv[])
 {
