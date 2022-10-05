63e8f0bf3a8e67be825e06c64933b0a3a5b56c54
sari107 <sari107@github.com>
2022-10-05 00:59:20 -0700

    Addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "ctype.h"
 /**
  *
  *
@@ -8,32 +9,37 @@
  *
  */
 
-int main(int agrc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 1;
+
 	int sum = 0;
-	 if(argc > 1)
-	 {
-		 while(i < argc)
-		 {
-			 if(isdigit(argv[i]))
-			 {
-				 sum = sum + argv[i];
-				 i++
-			 }
+	
+	if(argc <= 1)
+	{
+		printf("0\n");
+	}
 
-			 else
-			 {
-				 printf("Error\n");
-				 return (1);
-			 }
-		 }
-		 printf("%d\n", sum);
-	 }
+	else
+	{
 
-	 else
-	 {
-		 printf("0\n");
-	 }
-	 return (0);
+	for(int i=1;i < argc;i++)
+	{
+		if (atoi(argv[i]) != 0)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	
+	}
+	printf("%d\n", sum);
+	
+	}
+
+
+      return 0;
 }
