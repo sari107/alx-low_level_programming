2bd9f52e125d396cc5725a97b6e46c4d4e2a598e
sari107 <sari107@github.com>
2022-10-10 04:31:54 -0700

    srtncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,13 @@ char *_strncat(char *dest, char *src, int n)
 
 {
 
-	int i,j = 0;
+	int i = 0;
+	int j = 0;
 
 	while(*(dest + i) != '\0')
-	{
 		i++;
-	}
+
+
 		
 	while(*(src +j) != '\0' && j < n)
 	{
