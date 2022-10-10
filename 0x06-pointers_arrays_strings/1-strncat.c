a0bccbcfa77527dd94f37a98a92a6005a1c3bfa1
sari107 <sari107@github.com>
2022-10-10 04:34:01 -0700

    srtncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,8 @@
  * @dest : copies to
  * @src : copies from
  * @n : number of character to be copied
- * Return :Void
+ *
+ * Return : dest
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -15,12 +16,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while(*(dest + i) != '\0')
+	while (*(dest + i) != '\0')
 		i++;
 
 
 		
-	while(*(src +j) != '\0' && j < n)
+	while (*(src +j) != '\0' && j < n)
 	{
 		*(dest + i) = *(src + j);
 		i++;
