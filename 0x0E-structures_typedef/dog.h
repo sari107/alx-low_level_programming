83356723a518596a1d7b9f47ffedc2e520956ece
sari107 <sari107@github.com>
2022-10-11 00:13:22 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * struct - structure define dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * return: 0
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
