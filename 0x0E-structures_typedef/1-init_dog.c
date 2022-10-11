457a6aeed4f340dbfe760e3c8975f4b58d9ec55a
sari107 <sari107@github.com>
2022-10-11 05:08:30 -0700

    hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,15 +8,12 @@
  * @age: Age as float
  * @d: pointer
  */
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
- 	
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = ownwer;
 }
