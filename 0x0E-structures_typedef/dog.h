0723561350e894dd14f0f3cc4a213d45d241a04c
sari107 <sari107@github.com>
2022-10-11 04:51:24 -0700

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #include "stdlib.h"
 
+/**
+ * struct dog - list dates about pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
+ */
+
 struct dog
 {
 	char *name;
@@ -9,6 +16,9 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog - rename strucut with typedef
+ */
 typedef struct dog dog_t;
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
