f2adcbb57038f37bf6fe38b93f316e6420e81853
sari107 <sari107@github.com>
2022-10-20 05:20:39 -0700

    Header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,19 +26,18 @@
  */
 
 typedef struct list_s
+
 {
 
-		char *str;
+	char *str;
 
-		unsigned int len;
+	unsigned int len;
 
-		struct list_s *next;
+	struct list_s *next;
 
 } list_t;
 
-void __attribute__((constructor)) hare(void);
 
-void hare(void);
 
 size_t print_list(const list_t *h);
 
@@ -49,3 +48,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
+
+
+
+#endif
