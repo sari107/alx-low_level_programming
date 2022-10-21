2018da2e043f28527f761d1fa10785f60854c3c0
sari107 <sari107@github.com>
2022-10-20 22:27:43 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,12 +2,10 @@
 
 /**
  *_strcmp - Function which compare two strings and
- *
  *@s1: first string
- *
  *@s2:second string
- *
- *Return:returns zero if s1 == s2
+ *Return:
+ *       returns zero if s1 == s2
  *       returns negative number if s1 < s2
  *       returns positive number if s1 > s2
  */
