56bc67ba4378709e304390a28f291c5338438b79
sari107 <sari107@github.com>
2022-10-24 05:08:09 -0700

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *set_string - set the value of a pointerto char
+ *@s: the value to modifies
+ *@to: the value to assign
+ *Return: nothing
+ */
+
+void set_string(char **s, char *to)
+
+{
+
+	*s = to;
+
+}
