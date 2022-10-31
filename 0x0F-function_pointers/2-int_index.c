986844a3ec0a4416278745bb548ff54126cc1450
sari107 <sari107@github.com>
2022-10-31 05:37:38 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,45 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - ___
+ *@array: ___
+ *@size: ___
+ *@cmp: ___
+ *Return: ___
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+
+		int i = 0;
+
+
+
+			if (size > 0)
+
+					{
+
+								if (array != NULL && cmp != NULL)
+
+										{
+
+													while (i < size)
+
+																{
+
+																				if (cmp(array[i]))
+
+																									return (i);
+
+																							i++;
+
+																									}
+
+														}
+
+					}
+
+			return (-1);
+
+}
